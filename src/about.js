4c0697d2f2b23c6ccbf087c1fcbd703e267d858c
XRaiderGH <raigriffioen3@gmail.com>
2019-05-21 16:29:55 +0200

    added icons to the services page

--- src/about.js
+++ src/about.js
@@ -3,9 +3,36 @@ import React from 'react';
 function about(){
     return(
         <div className={'about'}>
-            <h1>hello</h1>
-            <div className="blackPageTransition" style={style1}>.
+
+            <div className="aboutOne">
+                <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                    <g id="Ellipse_10" data-name="Ellipse 10" fill="#fff" stroke="#707070" stroke-width="1">
+                        <circle cx="150" cy="150" r="150" stroke="none"/>
+                        <circle cx="150" cy="150" r="149.5" fill="none"/>
+                    </g>
+                </svg>
+                <div className='aboutOneText'>
+                    <h1>Rai Griffioen</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores, beatae, doloremque impedit incidunt ipsa laborum modi necessitatibus numquam odit omnis placeat praesentium recusandae repudiandae, vero voluptatem voluptates voluptatum. Quae.</p>
+                </div>
             </div>
+
+            <div className="aboutTwo">
+                <div className='aboutOneText'>
+                    <h1>Pim Hakkert</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores, beatae, doloremque impedit incidunt ipsa laborum modi necessitatibus numquam odit omnis placeat praesentium recusandae repudiandae, vero voluptatem voluptates voluptatum. Quae.</p>
+                </div>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                    <g id="Ellipse_10" data-name="Ellipse 10" fill="url('images/Image1.png')" stroke="#707070" stroke-width="1">
+                        <circle cx="150" cy="150" r="150" stroke="none"/>
+                        <circle cx="150" cy="150" r="149.5" fill="none"/>
+                    </g>
+                </svg>
+
+            </div>
+
+
         </div>
 
 
@@ -13,7 +40,7 @@ function about(){
 }
 
 var style1 = {
-    marginTop: "34em"
+    bottom : '0'
 };
 
 export default about;
