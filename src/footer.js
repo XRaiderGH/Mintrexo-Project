1ef9b08098d815008c8194f9dd5c4fe7ac5a7646
XRaiderGH <raigriffioen3@gmail.com>
2019-06-06 12:28:54 +0200

    updated portfolio page

--- src/footer.js
+++ src/footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './images/logo.png';
 
 
+
 function Footer(){
     return(
         <div className='footer'>
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,14 +1,121 @@
 import React from 'react';
+import Image2 from "./images/profilepic.jpg";
+import image1 from './images/Image1.png';
 
 
 function portfolio() {
     return (
         <div className='portfolio'>
            <div className="portfolioHeader">
-               <h1 className='portfolioHeaderTitle'>Portfolio</h1>
-               <article className='portfolioHeaderText'>
-                  Nothing to see here yet.
-               </article>
+
+               <div className="portfolioContainer">
+
+                   <div className="portfolioItem">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                           <defs>
+                               <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="100%" width="100%">
+                                   <image x="0" y="-30%" xlinkHref={image1} height="155%" width="155%" />
+                               </pattern>
+                           </defs>
+                           <g id="Ellipse_10" data-name="Ellipse 10" fill="url(#image)" stroke="#707070" strokeWidth="1">
+                               <circle cx="150" cy="150" r="150" stroke="url(#image)"/>
+                               <circle cx="150" cy="150" r="149.5" fill="url(#image)"/>
+                           </g>
+                       </svg>
+                       
+                       <div className="portfolioItemTitle">
+                            <h1>Title</h1>
+                       </div>
+
+                       <div className="portfolioItemContent">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, aut dicta dolor eaque exercitationem labore mollitia natus nihil, nisi non numquam pariatur, provident quibusdam quidem quo reprehenderit sed sequi sit!
+                       </div>
+
+                       <div className="portfolioItemLink">
+                           <a href=''>link</a>
+                       </div>
+                   </div>
+
+                   <div className="portfolioItem">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                           <defs>
+                               <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="100%" width="100%">
+                                   <image x="0" y="-30%" xlinkHref={image1} height="155%" width="155%" />
+                               </pattern>
+                           </defs>
+                           <g id="Ellipse_10" data-name="Ellipse 10" fill="url(#image)" stroke="#707070" strokeWidth="1">
+                               <circle cx="150" cy="150" r="150" stroke="url(#image)"/>
+                               <circle cx="150" cy="150" r="149.5" fill="url(#image)"/>
+                           </g>
+                       </svg>
+
+                       <div className="portfolioItemTitle">
+                           <h1>Title</h1>
+                       </div>
+
+                       <div className="portfolioItemContent">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, aut dicta dolor eaque exercitationem labore mollitia natus nihil, nisi non numquam pariatur, provident quibusdam quidem quo reprehenderit sed sequi sit!
+                       </div>
+
+                       <div className="portfolioItemLink">
+                           <a href=''>link</a>
+                       </div>
+                   </div>
+
+                   <div className="portfolioItem">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                           <defs>
+                               <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="100%" width="100%">
+                                   <image x="0" y="-30%" xlinkHref={image1} height="155%" width="155%" />
+                               </pattern>
+                           </defs>
+                           <g id="Ellipse_10" data-name="Ellipse 10" fill="url(#image)" stroke="#707070" strokeWidth="1">
+                               <circle cx="150" cy="150" r="150" stroke="url(#image)"/>
+                               <circle cx="150" cy="150" r="149.5" fill="url(#image)"/>
+                           </g>
+                       </svg>
+
+                       <div className="portfolioItemTitle">
+                           <h1>Title</h1>
+                       </div>
+
+                       <div className="portfolioItemContent">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, aut dicta dolor eaque exercitationem labore mollitia natus nihil, nisi non numquam pariatur, provident quibusdam quidem quo reprehenderit sed sequi sit!
+                       </div>
+
+                       <div className="portfolioItemLink">
+                           <a href=''>link</a>
+                       </div>
+                   </div>
+
+                   <div className="portfolioItem">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                           <defs>
+                               <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="100%" width="100%">
+                                   <image x="0" y="-30%" xlinkHref={image1} height="155%" width="155%" />
+                               </pattern>
+                           </defs>
+                           <g id="Ellipse_10" data-name="Ellipse 10" fill="url(#image)" stroke="#707070" strokeWidth="1">
+                               <circle cx="150" cy="150" r="150" stroke="url(#image)"/>
+                               <circle cx="150" cy="150" r="149.5" fill="url(#image)"/>
+                           </g>
+                       </svg>
+
+                       <div className="portfolioItemTitle">
+                           <h1>Title</h1>
+                       </div>
+
+                       <div className="portfolioItemContent">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, aut dicta dolor eaque exercitationem labore mollitia natus nihil, nisi non numquam pariatur, provident quibusdam quidem quo reprehenderit sed sequi sit!
+                       </div>
+
+                       <div className="portfolioItemLink">
+                           <a href=''>link</a>
+                       </div>
+                   </div>
+
+
+               </div>
            </div>
         </div>
     )
