82a541f33e3fdeccaf2fad897be3d027bad274a5
XRaiderGH <raigriffioen3@gmail.com>
2019-06-14 12:32:29 +0200

    Revert "Revert "w""
    This reverts commit bfd2e65fe34d924b83cc80dd0c533efeb6206d6b.

--- web_api/config/db.php
+++ web_api/config/db.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = 'mysql:dbname=mintrexo;host=127.0.0.1';
-$user = 'root';
-$password = '';
+$dsn = 'mysql:dbname=mintrexo_main;host=127.0.0.1';
+$user = 'mintrexo_root';
+$password = ',tI_BUeLUF}2';
 try {
     $database = new PDO($dsn, $user, $password);
 } catch (PDOException $e) {
