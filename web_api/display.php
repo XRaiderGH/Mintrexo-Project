ab3cbe093f77b231ff4b70849706a7c7901ec50a
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-06-14 17:11:09 +0200

    Added search articles method to API

---
+++ web_api/display.php
@@ -0,0 +1,48 @@
+<?php
+include 'config/db.php';
+
+function displayBlogposts() {
+    return json_encode(getBlogposts());
+}
+
+function getBlogposts() {
+    global $database;
+    $sql = "SELECT * FROM blogposts";
+    $sth = $database->prepare($sql);
+    $sth->execute();
+
+    $obj = (object) [
+        'blog' => $sth->fetchAll(PDO::FETCH_ASSOC)
+    ];
+    return ($obj);
+}
+
+function displayPortfolio() {
+    return json_encode(getPortfolio());
+}
+
+function getPortfolio() {
+    global $database;
+    $sql = "SELECT * FROM portfolio";
+    $sth = $database->prepare($sql);
+    $sth->execute();
+
+    $obj = (object) [
+        'portfolioItem' => $sth->fetchAll(PDO::FETCH_ASSOC)
+    ];
+    return ($obj);
+}
+
+function displayArticles() {
+    return json_encode(getArticles($text));
+}
+
+function getArticles($text) {
+    $database = dbConnect();
+    $text = htmlspecialchars($text);
+    $get_article = $database->prepare("SELECT * FROM blogposts WHERE titel LIKE concat('%' :titel, '%')");
+    $get_article->execute(array('titel' => $text));
+    $results = $get_article->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+}
+?>
--- web_api/index.php
+++ web_api/index.php
@@ -1,8 +1,13 @@
 <?php
-include ('display_blogposts.php');
+header("Access-Control-Allow-Origin: *");
+include ('display.php');
     if ($_SERVER['REQUEST_METHOD'] == "GET") {
         if($_GET['request']=='getBlogposts') {
             echo (displayBlogposts());
+        } elseif($_GET['request']=='getPortfolio') {
+            echo (displayPortfolio());
+        } elseif($_GET['request']=='getArticles' && !empty($_GET['article'])) {
+            echo (displayArticles($_GET['article']));
         } else {
             echo 'API ERROR: Wrong request';
         }
